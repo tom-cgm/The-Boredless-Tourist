ba1a351bafb61458d94ccd4c3616219e36c87bf8
Tom Carlos <tcgurmangit@gmail.com>
2020-02-11 01:42:06 +0000

    added test

--- tourist.py
+++ tourist.py
@@ -1,3 +1,13 @@
 #The Boredless Tourist
 #Project from Codecademy computer science pathway
-#""
+
+#Now let’s create some data that we’re going to use to test the functionality that we create for The Boredless Tourist.
+#The first is our list of destinations that we’re going to be using.
+
+# List of destinations
+destinations = ["Paris, Frace","Shanghai, China","Los Angeles, USA","Sao Paulo, Brazil","Cairo, Egypt"]
+
+#Test traveller to see if functionality is working
+test_traveller = ['Erin Wilkes','Shanghai, China', ['historical site', 'art']]
+# "This is a traveler (a user of The Boredless Tourist application) whose name is Erin Wilkes who likes ...
+# historical buildings and art. Erin is in China right now, hopefully we can find some good places to show her."
